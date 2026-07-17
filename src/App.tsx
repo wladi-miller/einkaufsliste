107bc96024dc253d91ee3176ae810189a8fe8390
Wladimir Miller <wladim.miller@gmail.com>
2026-07-17 18:31:13 +0200

    Add basic shopping list layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="min-h-screen bg-gray-100 flex justify-center items-center">
-      <section className="w-full max-w-md rounded-xl bg-white shadow-lg p-6">
+    <main className="flex min-h-screen items-center justify-center bg-gray-100 p-4">
+      <section className="w-full max-w-md rounded-xl bg-white p-6 shadow-lg">
         <h1 className="text-3xl font-bold">Einkaufsliste</h1>
       </section>
     </main>
