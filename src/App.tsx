18b98a70b7be1e6d5e3eafd74c22c54bd2915797
Wladimir Miller <wladim.miller@gmail.com>
2026-07-17 18:20:15 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <section className="w-full max-w-md rounded-xl bg-white shadow-lg p-6">
+        <h1 className="text-3xl font-bold">Einkaufsliste</h1>
+      </section>
+    </main>
+  );
+}
+
+export default App;
